b4ea141a05cc1b702a68e8a5d5cb5731582dd500
Kerjox <kerjoxgamer095@gmail.com>
2020-03-17 21:47:29 +0100

    Update FermentadorNodeMCUV1.ino

--- FermentadorNodeMCUV1/FermentadorNodeMCUV1.ino
+++ FermentadorNodeMCUV1/FermentadorNodeMCUV1.ino
@@ -476,7 +476,7 @@ void fermentacion(){
       Serial.print(hour(tiempoRestante));      // funcion que obtiene la hora de la fecha completa
       Serial.print(":");       // caracter dos puntos como separador
       Serial.println(minute(tiempoRestante));      // funcion que obtiene los minutos de la fecha completa
-      delay(1000);
+      delay(60000);
     }while(!fin);
 //PUESTA A CERO FINAL
   tiempoi = 0;
