86976ea206acc98ca7068c5d52d30c7b7aded53f
Kerjox <60511422+Kerjox@users.noreply.github.com>
2020-04-14 11:09:53 +0200

    Delete pedirdatosPost.php

--- cerveceroWebsite/php/arduino/pedirdatosPost.php
+++
@@ -1,8 +0,0 @@
-<?php
-require '../conexion.php';
-$IDreceta = $_POST['IDreceta'];
-$sql = $conn->query("SELECT * FROM recetas WHERE id=$IDreceta");
-while ($valores = mysqli_fetch_array($sql)) {
-    echo "nombre=$valores[nombre];tempMacer=$valores[tempMacer];tiempoMacer=$valores[tiempoMacer];tempCoc=$valores[tempCoc];tiempoCoc=$valores[tiempoCoc];tempFermen=$valores[tempFermen];tiempoFermen=$valores[tiempoFermen]";
-}
-?>
