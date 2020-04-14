04087b9d436bf261602e3b4972a703ece32187d4
Kerjox <60511422+Kerjox@users.noreply.github.com>
2020-04-14 11:08:54 +0200

    Delete checkCancel.php

--- cerveceroWebsite/php/arduino/checkCancel.php
+++
@@ -1,9 +0,0 @@
-<?php
-require '../conexion.php';
-$IDplaca = $_GET['IDplaca'];
-$sql = $conn->query("SELECT cancelar FROM menu WHERE IDplaca = $IDplaca");
-while ($valores = mysqli_fetch_array($sql)) {
-    echo "$valores[cancelar]";
-}
-
-?>
